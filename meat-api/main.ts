6fb84713e173b4bd586ce59e818b6045956ddcb1
Bráulio Figueiredo Pinto <braulio@braulioti.com.br>
2018-07-20 02:22:44 -0300

    sessão 3 aula 18

--- meat-api/main.ts
+++ meat-api/main.ts
@@ -1,41 +1,11 @@
-import * as restify from 'restify';
+import {Server} from './server/server';
 
-const server = restify.createServer({
-    name: 'meat-api',
-    version: '1.0.0'
-});
+const server = new Server();
 
-server.use(restify.plugins.queryParser());
-
-server.get('/info', [
-    (req, resp, next) => {
-        if (req.userAgent() && req.userAgent().includes('MSIE 7.0')) {
-            // resp.status(400);
-            // resp.json({ message: 'Please, update your browser' });
-            let error: any = new Error();
-            error.statusCode = 400;
-            error.message = 'Please, update your browser';
-            return next(error);
-        }
-        return next();
-    },
-    (req, resp, next) => {
-        // resp.contentType = 'application/json';
-        // resp.status(400);
-        // resp.setHeader('Content-Type', 'application/json');
-        // resp.send({ message: 'Hello' });
-        resp.json({
-            browser: req.userAgent(),
-            method: req.method,
-            url: req.href(),
-            path: req.path(),
-            query: req.query
-        });
-
-        return next();
-    }]
-);
-
-server.listen(3000, () => {
-    console.log('API is running on http://localhost:3000');
+server.bootstrap().then(server => {
+    console.log('Server is listening on port ' + server.application.address().port);
+}).catch(error => {
+    console.log('Server failed to start');
+    console.error(error);
+    process.exit(1);
 });
