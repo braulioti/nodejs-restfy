25d61919ec51493451368b15bc4a7a5543cff6c8
Bráulio Figueiredo Pinto <braulio@braulioti.com.br>
2018-07-21 21:46:42 -0300

    sessão 5 aula 31

--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -66,7 +66,19 @@ class UsersRouter extends Router {
                 resp.send(404);
             });
 
-        })
+        });
+
+        application.del('/users/:id', (req, resp, next) => {
+            User.remove({ _id: req.params.id }).exec()
+                .then((cmdResult: any) => {
+                if (cmdResult.result.n) {
+                    resp.send(204);
+                } else {
+                    resp.send(404);
+                }
+                return next();
+            });
+        });
     }
 }
 
