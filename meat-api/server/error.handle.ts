97af1e9a660d80f6c968c7d3f33eb041848d2ef9
Bráulio Figueiredo Pinto <braulio@braulioti.com.br>
2018-07-22 14:53:46 -0300

    sessão 6 aula 34

--- meat-api/server/error.handle.ts
+++ meat-api/server/error.handle.ts
@@ -16,6 +16,13 @@ export const handleError = (req: restify.Request, resp: restify.Response, err, d
 
         case 'ValidationError':
             err.statusCode = 400;
+            const messages: any[] = [];
+            for (let name in err.errors) {
+                messages.push({message: err.errors[name].message});
+            }
+            err.toJSON = () => ({
+               errors: messages
+            });
             break;
     }
 
