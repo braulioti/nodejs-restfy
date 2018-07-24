ae1fb872b6a42865b0dce56f19ffadf8cfc678ce
Bráulio Figueiredo Pinto <braulio@braulioti.com.br>
2018-07-24 07:17:59 -0300

    sessão 7 aula 38

--- meat-api/users/users.router.ts
+++ meat-api/users/users.router.ts
@@ -1,12 +1,11 @@
-import {Router} from '../common/router';
 import * as restify from 'restify';
 import {User} from './users.model';
-import {NotFoundError} from 'restify-errors';
+import {ModelRouter} from '../common/model-router';
 
-class UsersRouter extends Router {
+class UsersRouter extends ModelRouter<User> {
 
     constructor() {
-        super();
+        super(User);
         this.on('beforeRender', document => {
             document.password = undefined;
             // delete document.password
@@ -14,64 +13,12 @@ class UsersRouter extends Router {
     }
 
     applyRoutes(application: restify.Server) {
-        application.get('/users', (req, resp, next) => {
-            User.find().then(this.render(resp, next))
-                .catch(next);
-        });
-
-        application.get('/users/:id', (req, resp, next) => {
-            User.findById(req.params.id).then(this.render(resp, next))
-                .catch(next);
-        });
-
-        application.post('/users', (req, resp, next) => {
-            let user = new User(req.body);
-
-            user.save().then(this.render(resp, next))
-                .catch(next);
-        });
-
-        application.put('/users/:id', (req, resp, next) => {
-            const options = {
-                override: true,
-                runValidators: true
-            };
-
-            User.update({ _id: req.params.id }, req.body, options).exec()
-                .then(result => {
-                    if (result.n) {
-                        return User.findById(req.params.id);
-                    } else {
-                        throw new NotFoundError('Documento não encontrado');
-                    }
-            }).then(this.render(resp, next))
-                .catch(next);
-        });
-
-        application.patch('/users/:id', (req, resp, next) => {
-            const options = {
-                new: true,
-                runValidators: true
-            };
-
-            User.findByIdAndUpdate(req.params.id, req.body, options)
-                .then(this.render(resp, next))
-                .catch(next);
-
-        });
-
-        application.del('/users/:id', (req, resp, next) => {
-            User.remove({ _id: req.params.id }).exec()
-                .then((cmdResult: any) => {
-                    if (cmdResult.result.n) {
-                        resp.send(204);
-                    } else {
-                        throw new NotFoundError('Documento não encontrado');
-                    }
-                    return next();
-                })
-                .catch(next);
-        });
+        application.get('/users', this.findAll);
+        application.get('/users/:id', [this.validateId, this.findById]);
+        application.post('/users', this.save);
+        application.put('/users/:id', [this.validateId, this.replace]);
+        application.patch('/users/:id', [this.validateId, this.update]);
+        application.del('/users/:id', [this.validateId, this.delete]);
     }
 }
 
