121dc845d3eb4da605d2c7745a1458a17c698f85
Bráulio Figueiredo Pinto <braulio@braulioti.com.br>
2018-07-29 03:12:26 -0300

    sessão 10 aula 50

--- meat-api/users/users.test.ts
+++ meat-api/users/users.test.ts
@@ -44,6 +44,39 @@ test('post /users', () => {
         }).catch(fail);
 });
 
+test('get /users/aaaaa - not found', () => {
+    return request(address).get('/users/aaaaa')
+        .then(response => {
+            expect(response.status).toBe(404);
+        }).catch(fail);
+});
+
+test('patch /users/:id', () => {
+    return request(address)
+        .post('/users')
+        .send({
+            name: 'usuario2',
+            email: 'usuario2@email.com',
+            password: '123456'
+        })
+        .then(response =>
+            request(address)
+                .patch(`/users/${response.body._id}`)
+                .send({
+                    name: 'usuario2 - patch'
+                }
+            )
+        )
+        .then(response => {
+            expect(response.status).toBe(200);
+            expect(response.body._id).toBeDefined();
+            expect(response.body.name).toBe('usuario2 - patch');
+            expect(response.body.email).toBe('usuario2@email.com');
+            expect(response.body.password).toBeUndefined();
+        })
+        .catch(fail);
+});
+
 afterAll(() => {
     return server.shutdown();
 });
